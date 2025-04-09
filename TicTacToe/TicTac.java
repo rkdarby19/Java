7941291561936034fbdbd043e7b7b57302facc42
Richard Darby <rkellindarby@gmail.com>
2025-04-09 15:59:03 -0700

    Completed printBoard func, prints blank board. Need testing to confirm player spots print properly

--- TicTacToe/TicTac.java
+++ TicTacToe/TicTac.java
@@ -6,7 +6,7 @@ public class TicTac{
 			       0,0,0,
 			       0,0,0};
 
-			
+		printBoard(spots);	
 
 	}
 	public static void printBoard(int[] spots){
@@ -26,12 +26,87 @@ public class TicTac{
 				0,0,0,1,0,0,0,1,0,0,0,2,
 				3,3,3,3,3,3,3,3,3,3,3,2,
 				0,0,0,1,0,0,0,1,0,0,0,2};
-		for(int i = 0; i < spots.length; i++){
-			if(spots[i] != ){
-				if(spots[i] == 1){
 
-				}
+		if(spots[0] == 1){
+			spaces[1] = 4;
+		}
+		if(spots[0] == 2){
+			spaces[1] = 5;
+		}
+		if(spots[1] == 1){
+			spaces[5] = 4;
+		}
+		if(spots[1] == 2){
+			spaces[5] = 5;
+		}
+		if(spots[2] == 1){
+			spaces[9] = 4;
+		}
+		if(spots[2] == 2){
+			spaces[9] = 5;
+		}
+		if(spots[3] == 1){
+			spaces[25] = 4;
+		}
+		if(spots[3] == 2){
+			spaces[25] = 5;
+		}
+		if(spots[4] == 1){
+			spaces[29] = 4;
+		}
+		if(spots[4] == 2){
+			spaces[29] = 5;
+		}
+		if(spots[5] == 1){
+			spaces[33] = 4;
+		}
+		if(spots[5] == 2){
+			spaces[33] = 5;
+		}
+		if(spots[6] == 1){
+			spaces[49] = 4;
+		}
+		if(spots[6] == 2){
+			spaces[49] = 5;
+		}
+		if(spots[7] == 1){
+			spaces[53] = 4;
+		}
+		if(spots[7] == 2){
+			spaces[53] = 5;
+		}
+		if(spots[8] == 1){
+			spaces[57] = 4;
+		}
+		if(spots[8] == 2){
+			spaces[57] = 5;
+		}
+
+		for(int i = 0; i < spaces.length; i++){
+			switch (spaces[i]){
+				case 0:
+					System.out.print(" ");
+					break;
+				case 1:
+					System.out.print("|");
+					break;
+				case 2:
+					System.out.println();
+					break;
+				case 3:
+					System.out.print("-");
+					break;
+				case 4:
+					System.out.print("X");
+					break;
+				case 5:
+					System.out.print("O");
+					break;
+				default:
+					System.out.println("ERROR");
+					break;
 			}
 		}
+
 	}
 }
